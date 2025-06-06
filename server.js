2bb1ccbd71b0e03f6023ba782275b54c2f99fed1
VishalDhanasekaran <vishal@localhost.localdomain>
2025-06-06 16:20:50 +0530

    server

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+//const fetch = require('node-fetch');
+
+const app = express();
+app.use((cors()));
+
+app.get('/reddit/:subreddit', async (req, res)=>{
+
+    const subreddit = req.params.subreddit;
+    console.log('idhu dhan request uh ', subreddit)
+    try
+    {
+        const response = await fetch(`https://www.reddit.com/r/${subreddit}.json`);
+        const data = await response.json();
+        res.json(data);
+    }
+    catch(error)
+    {
+        res.status(500).json({error:"faile to fetch subreddit"});
+    }
+});
+
+app.listen(5000, ()=>{
+    console.log('Proxy server is running on port 5000...')
+})
