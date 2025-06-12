50a84fa7a29d19f68b304dbd404bb2a68536a737
VishalDhanasekaran <2005vishaldhanasekaran@gmail.com>
2025-06-12 21:47:15 +0530

    perf!: sever

--- api/server.js
+++ api/server.js
@@ -1,24 +1,20 @@
-const fetch = require('node-fetch');
+const express = require("express")
+const cors = require('cors')
 
-module.exports = async (req, res) => {
-  const { query } = req;
-  const subreddit = query.subreddit;
+const app = express();
 
-  if (!subreddit) 
-  {
-    return res.status(400).json({ error: 'Subreddit parameter is missing' });
-  }
+app.get('/reddit/:subreddit', async (req, res)=>{
 
-  try 
-  {
-    console.log("Request received for subreddit:", subreddit);
+  const subreddit = req.params.subreddit;
+
+  try{
     const response = await fetch(`https://www.reddit.com/r/${subreddit}.json`);
-    const data = await response.json();
-    return res.status(200).json(data);
-  } 
-  catch (error) 
-  {
-    console.error("Error fetching subreddit:", error.message);
-    return res.status(500).json({ error: 'Failed to fetch subreddit' });
+
+    const data = response.json();
+    res.json(data);
+
   }
-};
+  catch{
+    res.status(500).json({error: "failed to fetch subreddit!"})
+  }
+})
