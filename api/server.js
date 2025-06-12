1ee61b7ea7b10951c98edf25fe594a9720cb989e
VishalDhanasekaran <2005vishaldhanasekaran@gmail.com>
2025-06-12 20:39:33 +0530

    style: indentations

--- api/server.js
+++ api/server.js
@@ -9,12 +9,15 @@ module.exports = async (req, res) => {
     return res.status(400).json({ error: 'Subreddit parameter is missing' });
   }
 
-  try {
+  try 
+  {
     console.log("Request received for subreddit:", subreddit);
     const response = await fetch(`https://www.reddit.com/r/${subreddit}.json`);
     const data = await response.json();
     return res.status(200).json(data);
-  } catch (error) {
+  } 
+  catch (error) 
+  {
     console.error("Error fetching subreddit:", error.message);
     return res.status(500).json({ error: 'Failed to fetch subreddit' });
   }
